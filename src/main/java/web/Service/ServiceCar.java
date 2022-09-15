439b6ba400563ceb0a44b3d389d3c5b5b63b3a2b
Aaahooly <andreygluh@yandex.ru>
2022-09-15 17:12:11 +0300

    Без костылей в методе index.

--- src/main/java/web/Service/ServiceCar.java
+++ src/main/java/web/Service/ServiceCar.java
@@ -1,23 +1,29 @@
 package web.Service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import web.dao.CarDao;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 @Component
 public class ServiceCar {
 
-    private final CarDao carDao;
+    private final List<Car> carList;
 
-    @Autowired
-    public ServiceCar(CarDao carDao) {
-        this.carDao = carDao;
+    {
+        carList = new ArrayList<>();
+        carList.add(new Car("Honda", "Civic", 2006));
+        carList.add(new Car("Chevrolet", "Cruze", 2008));
+        carList.add(new Car("Toyota", "Camry", 2018));
+        carList.add(new Car("Nissan", "Juke", 2000));
+        carList.add(new Car("Lada", "Vesta", 2022));
     }
 
     public List<Car> index(Integer count) {
-        return carDao.index(count);
+    if (count == null || !(count >= 5)) {
+            return carList;
+        } else return carList.subList(0, count);
     }
 }
--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,11 +1,9 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import web.Service.ServiceCar;
-import web.dao.CarDao;
 
 @Controller
 @RequestMapping("/cars")
